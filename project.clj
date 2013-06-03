a1dec37772819ee1583f58bc056b2fbf0c1304a3
Joshua Miller <elefantstn@gmail.com>
2013-06-03 09:40:28 -0400

    Upgrade Clojure version.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject pa511 "0.1.2"
+(defproject pa511 "0.1.3"
   :description "Interpreting PA 511 Data"
   :url "http://github.com/joshuamiller/pa511"
+  :min-lein-version "2.0.0"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.6.3"]
-                 [org.clojure/data.xml "0.0.6"]
+                 [org.clojure/data.xml "0.0.7"]
                  [clj-time "0.4.4"]])
